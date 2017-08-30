99d5ab2b9392281a724d53a7a6cfe7f7ea5f26f0
norajs <norajsinger@gmail.com>
2017-08-30 14:27:21 -0400

    Added second page, projects.php

--- projects.php
+++ projects.php
@@ -9,12 +9,131 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
       
     <link rel="stylesheet" href="style.css" >
+      
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+      
   </head>
   <body>
-   
+      
+      
+      <nav id="header" class="navbar fixed-top navbar-light bg-faded">
+        <a class="navbar-brand" href="/norajs.com/"><img id="logo" src="skinnylogo.png" ></a>
+          
+  
+      
+      </nav>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+   <div class= container id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="card">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+        <a class="collapsed title" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <i class="fa fa-anchor" aria-hidden="true"></i>    CS50 Final Project 2016
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+      <div class="card-block">
+        <a href="projects/30MinsADay/" target="_blank">30 Mins A Day - CS50 Final Project</a>
+</br></br>
+<a href="https://www.youtube.com/watch?v=TM4DgI6hlUA&t=17s" target="_blank">30 Mins A Day - Youtube Demo</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed title" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <i class="fa fa-anchor" aria-hidden="true"></i>   Gym Vermilion 
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="card-block">
+        <a href="http://www.gymvermilion.com/" target="_blank">Gym Vermilion</a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed title" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <i class="fa fa-anchor" aria-hidden="true"></i>   Coder 
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="card-block">
+        <a href="/projects/Coder/" target="_blank">Coder</a>
+         
+      </div>
+    </div>
+  </div>
+       
+       <div class="card">
+    <div class="card-header" role="tab" id="headingFour">
+      <h5 class="mb-0">
+        <a class="title" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+          <i class="fa fa-anchor" aria-hidden="true"></i>   A Bootsrap Landing Page
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="card-block">
+        <a href="/projects/BLanding/" target="_blank">A Bootsrap Landing Page template for mobile apps</a>
+      </div>
+    </div>
+  </div>
+</div>
+      
+      
+      
+      
+      
+      
+      
+      
+    <nav id="footer" class="navbar fixed-bottom navbar-light bg-faded">
+        
+          <div class="row">
+            <div class="col contact">
+                786.897.7037
+            </div>
+            <div class="col contact right">
+                norajsinger@gmail.com
+            </div>
+          </div>
+  
+      
+      </nav>
+      
+      
+      
+      
+      
       
     
-    
+      
+      
+      
+      
+      
+      
+      
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
